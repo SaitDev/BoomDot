ea9e6ef1413b020402c68c553ea6e503b2df9c50
Sait™ <sait.glacialdevil@gmail.com>
2016-04-20 21:32:03 +0700

    Part 2

--- play.pas
+++ play.pas
@@ -21,6 +21,8 @@ type
     Label1: TLabel;
     Timer1: TTimer;
     Timer2: TTimer;
+    Timer3: TTimer;
+    Timer4: TTimer;
     procedure FormShow(Sender: TObject);
     procedure Image1Click(Sender: TObject);
     procedure Image2Click(Sender: TObject);
@@ -29,6 +31,8 @@ type
     procedure Image5Click(Sender: TObject);
     procedure Timer1Timer(Sender: TObject);
     procedure Timer2Timer(Sender: TObject);
+    procedure Timer3Timer(Sender: TObject);
+    procedure Timer4Timer(Sender: TObject);
   private
     { private declarations }
   public
@@ -47,6 +51,7 @@ implementation
 var
   targetSpeed, dotSpeed, score: Integer;
   targetToLeft: Boolean;
+  backgroudSpeed, backgroupTop, dropDownSpeed: Real;
 
 procedure TPlayScreen.Timer1Timer(Sender: TObject);
 begin
@@ -65,17 +70,22 @@ end;
 
 procedure TPlayScreen.Timer2Timer(Sender: TObject);
 var
-  r : Integer;
+  distance : Real;
 begin
   Image5.Top:=Image5.Top-dotSpeed;
-  r:= sqr(Image5.Left+16 - (Image4.Left+24))
-      + sqr(Image5.Top + 16 - (Image4.Top+24));
+  distance:= sqrt(sqr(Image5.Left+16 - (Image4.Left+24))
+             + sqr(Image5.Top + 16 - (Image4.Top+24)));
   // Ghi điểm / Scored
-  if sqrt(r) <= 40 then
+  if distance <= 40 then
   begin
-    Image5.Top:=425;
+    //Image5.Top:=425;
     Timer2.Enabled:=False;
     sndPlaySound('Sounds\Scored.wav',SND_ASYNC or SND_NODEFAULT);
+    backgroudSpeed:=22;
+    dotSpeed:=15;
+    Image4.Hide;
+    Timer1.Enabled:=False;
+    Timer3.Enabled:=True;
     Inc(score);
     Label1.Caption:=IntToStr(score);
     Image4.Top:=200 + Random(150);
@@ -90,16 +100,57 @@ begin
     end;
 end;
 
+procedure TPlayScreen.Timer3Timer(Sender: TObject);
+begin
+  if dotSpeed>1 then
+  begin
+    if Image5.Top>=420 then dotSpeed:=3
+    else if Image5.Top>=400 then dotSpeed:=5;
+    if Image5.Top+dotSpeed>=430 then
+    begin
+      dotSpeed:=0;
+      backgroudSpeed:=0.4;
+      Image5.Top:=430;
+      Image4.Top:=150 + Random(150);
+      Image4.Left:=30+Random(290);
+      Timer3.Enabled:=False;
+      dropDownSpeed:=6.5;
+      Image4.Show;
+      Timer1.Enabled:=True;
+      Timer4.Enabled:=True;
+    end
+    else Image5.Top:=Image5.Top+dotSpeed;
+  end;
+  backgroupTop:=backgroupTop+backgroudSpeed;
+  Image1.Top:=round(backgroupTop);
+  if Image1.Top>=0 then backgroupTop:=-110;
+end;
+
+procedure TPlayScreen.Timer4Timer(Sender: TObject);
+begin
+  if dropDownSpeed>=1 then
+  begin
+    Image4.Top:=Image4.Top + round(dropDownSpeed);
+    dropDownSpeed:=dropDownSpeed - 0.5;
+  end
+  else begin
+      Timer4.Enabled:=False;
+  end;
+end;
+
 procedure TPlayScreen.FormShow(Sender: TObject);
 begin
    targetSpeed:=4;
-   dotSpeed:=12;
+   dotSpeed:=10;
    score:=0;
+   backgroupTop:=-110;
+   backgroudSpeed:=22;
    Randomize;
 end;
 
 procedure TPlayScreen.Image1Click(Sender: TObject);
 begin
+  dotSpeed:=10;
   Timer2.Enabled:=True;
 end;
 
